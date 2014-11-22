5514376961af5fe55e7ac58e4ddb5346effa3271
andrecun <andreas.hadiyono@gmail.com>
2014-11-22 15:07:38 +0700

    template hapus

--- template/header.php
+++
@@ -1,6 +0,0 @@
-<!-- Main component for a primary marketing message or call to action -->
-<div class="presidenRI-header">
-    <img src="<?php echo $url_rewrite;?>/assets/img/header.jpg" class="img-responsive">  
-    <?php include('navbar.php'); ?>
-
-</div>
